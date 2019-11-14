d966e13ed48a4eb0fb01be810f37d46ddd5e34f9
maslanp4tty <57738032+maslanp4tty@users.noreply.github.com>
2019-11-13 19:05:57 -0800

    Tugas 2

---
+++ pgame-16061/code_tugas2.py
@@ -0,0 +1,7 @@
+# Ini Tugas 2. Praktik Git untuk MK Pemrograman Game
+
+print (" my name is maslan m . patty") #Halo, nama saya maslan m. patty
+print ("I enjoyed studying at your ummu") #Saya senang kuliah di ummu
+#baris ini tidak akan dieksekusi
+print ("Halo Anak Informatika...") #baris ini dieksekusi
+#Baris ini juga merupakan Baris Komentar 
